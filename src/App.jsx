f82444c8a4500269ac90910fd70b729c2421ae59
SarveshLimaye <sarveshlimaye2002@gmail.com>
2023-12-08 22:00:44 +0530

    social auth using safe auth-kit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AuthProvider from "./context/auth";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home/Home";
 import CreateDAO from "./pages/CreateDAO/CreateDAO";
@@ -7,14 +8,16 @@ import RegisterUser from "./pages/RegisterUser/RegisterUser";
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/create-dao" element={<CreateDAO />} />
-        <Route path="/register" element={<RegisterUser />} />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create-dao" element={<CreateDAO />} />
+          <Route path="/register" element={<RegisterUser />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import { useContext } from "react";
+import { AuthContext } from "../../context/auth";
 
 const Home = () => {
-  return <div>Home</div>;
+  const {
+    safeAuthPack,
+    safeAuthSignInResponse,
+    setSafeAuthSignInResponse,
+    setIsAuthenticated,
+  } = useContext(AuthContext);
+
+  const login = async () => {
+    const signInInfo = await safeAuthPack?.signIn();
+
+    setSafeAuthSignInResponse(signInInfo);
+    setIsAuthenticated(true);
+  };
+
+  const logout = async () => {
+    await safeAuthPack?.signOut();
+
+    setSafeAuthSignInResponse(null);
+  };
+  return (
+    <>
+      {safeAuthSignInResponse?.eoa ? (
+        <>
+          <button onClick={logout}>Sign Out</button>
+          <p>{safeAuthSignInResponse?.eoa}</p>
+        </>
+      ) : (
+        <button onClick={login}>Sign In</button>
+      )}
+    </>
+  );
 };
 
 export default Home;
