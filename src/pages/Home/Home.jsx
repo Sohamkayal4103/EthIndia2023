9e493b760a7d676135407b9eb37a17f37b7257aa
Sarvesh Limaye <74766567+SarveshLimaye@users.noreply.github.com>
2023-12-09 00:34:08 +0530

    Revert "deployed safe account"

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,5 @@
 import { useContext } from "react";
-import { ethers } from "ethers";
 import { AuthContext } from "../../context/auth";
-import Safe, { SafeFactory, EthersAdapter } from "@safe-global/protocol-kit";
 
 const Home = () => {
   const {
@@ -11,47 +9,11 @@ const Home = () => {
     setIsAuthenticated,
   } = useContext(AuthContext);
 
-  const deploySafe = async (owner) => {
-    console.log(owner);
-    const provider = new ethers.providers.Web3Provider(
-      safeAuthPack?.getProvider()
-    );
-
-    const signer = provider.getSigner();
-
-    const ethAdapter = new EthersAdapter({
-      ethers,
-      signerOrProvider: signer,
-    });
-
-    const safeFactory = await SafeFactory.create({ ethAdapter });
-
-    const owners = [owner];
-
-    const threshold = 1;
-
-    const safeAccountConfig = {
-      owners,
-      threshold,
-    };
-
-    const safe = await safeFactory.deploySafe({
-      safeAccountConfig,
-    });
-
-    console.log(safe);
-    console.log("Safe deployed");
-  };
-
   const login = async () => {
     const signInInfo = await safeAuthPack?.signIn();
 
     setSafeAuthSignInResponse(signInInfo);
     setIsAuthenticated(true);
-
-    console.log("Deploying Safe");
-    const res = await deploySafe(signInInfo?.eoa);
-    console.log(res);
   };
 
   const logout = async () => {
