322a9a7ec89001faa5fd7e7d9d0c904db5038253
SarveshLimaye <sarveshlimaye2002@gmail.com>
2023-12-09 07:45:42 +0530

    explore page done using safe

--- src/pages/Explore/Explore.jsx
+++ src/pages/Explore/Explore.jsx
@@ -1,4 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { AuthContext } from "../../context/auth";
+
 import { ethers } from "ethers";
 import UserSideAbi from "../../utils/contractabis/UserSideAbi.json";
 import GovernanceTokenAbi from "../../utils/contractabis/GovernanceTokenAbi.json";
@@ -42,6 +44,8 @@ const Explore = () => {
   const [daoArray, setDaoArray] = useState([]);
   const [totalDaos, setTotalDaos] = useState(0);
 
+  const { safeAuthPack, safeAuthSignInResponse } = useContext(AuthContext);
+
   const getAllDaos = async () => {
     if (window.ethereum._state.accounts.length !== 0) {
       const provider = new ethers.providers.Web3Provider(window.ethereum);
@@ -79,12 +83,55 @@ const Explore = () => {
         ]);
         //console.log(daoData);
       }
+    } else {
+      const provider = new ethers.providers.Web3Provider(
+        safeAuthPack?.getProvider()
+      );
+
+      const signer = provider.getSigner();
+
+      const userSideContract = new ethers.Contract(
+        "0x7919303D9772b331F446e4eD2D1F20d1a9592CDE",
+        UserSideAbi,
+        signer
+      );
+
+      const tempTotal = Number(await userSideContract.totalDaos());
+
+      console.log(tempTotal);
+      let daoData,
+        tokenData,
+        governanceTokenContract,
+        govtTokenName,
+        govtTokenSymbol;
+      for (let i = 1; i <= tempTotal; i++) {
+        daoData = await userSideContract.daoIdtoDao(i);
+        governanceTokenContract = new ethers.Contract(
+          daoData.joiningTokenAddress,
+          GovernanceTokenAbi,
+          signer
+        );
+        govtTokenName = await governanceTokenContract.name();
+        govtTokenSymbol = await governanceTokenContract.symbol();
+        //console.log(govtTokenName + " " + govtTokenSymbol);
+        setDaoArray((prevState) => [
+          ...prevState,
+          {
+            daoData: daoData,
+            tokenName: govtTokenName,
+            tokenSymbol: govtTokenSymbol,
+          },
+        ]);
+        //console.log(daoData);
+      }
     }
   };
 
   useEffect(() => {
     return () => getAllDaos();
   }, []);
+
+  console.log(daoArray);
 
   return (
     <Box>
